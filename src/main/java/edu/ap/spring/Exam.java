2e05c3895e2b600e0da9ed686856c19aeffa5d6e
FFLP <ferdi_9678@hotmail.com>
2018-04-18 15:44:24 +0200

    Commit 1
    Working prime and coordinates

--- src/main/java/edu/ap/spring/Exam.java
+++ src/main/java/edu/ap/spring/Exam.java
@@ -2,6 +2,12 @@ package edu.ap.spring;
 
 import java.awt.Point;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,21 +17,27 @@ public class Exam {
 	// priemgetallen terug te geven
 	// 2 punten
 	public int[] getPrimes(int[] numbers) {
-		
+		return Arrays.stream(numbers)
+				.filter(x -> IntStream.rangeClosed(2, (int) Math.sqrt(x)).noneMatch(i -> x%i == 0))
+				.toArray();
 	}
 	
 	// Maak gebruik van lambdas en streams alle lowercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countLowercaseCharacters(String string) {
-        
+		System.out.println("test");
+		string.chars().stream()
 	}
 	
 	// Maak gebruik van lambdas en streams om de som van alle
 	// x-coordinaten uit de lijst van points te berekenen
 	// 1 punt
 	public int sumOfX(List<Point> points) {
-		
+		return points.stream()
+				.map(x -> x.getX())
+				.mapToInt(i -> i.intValue())
+				.sum();
 	}
 	
 	// Maak gebruik van lambdas en streams om een comma-separated
@@ -33,6 +45,6 @@ public class Exam {
 	// zijn aan twee
 	// 2 punten
 	public String getXOverTwo(List<Point> points) {
-		
+		return "hello";
 	}
 }
